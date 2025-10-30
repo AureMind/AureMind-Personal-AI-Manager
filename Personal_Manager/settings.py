526279debcb3fc61ec5fb1f6aed644e95e4efc0f
Prashant Nath <144515759+NathPrashant@users.noreply.github.com>
2025-10-30 20:19:01 +0530

    add .env for api

--- Personal_Manager/settings.py
+++ Personal_Manager/settings.py
@@ -12,17 +12,19 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 from pathlib import Path
 import os
-from .api import api
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+load_dotenv(os.path.join(BASE_DIR, '.env'))
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-=haim(vv-o54rd4$aeu&^^2-rutmqa8ya_^&$&=sa3hs!9oxcs'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 FERNET_KEY = b'8FwtQCZ1Qd6ozeREDK3Co_sUhr7J2PDbzrgFUvWFvdA='
 
 # SECURITY WARNING: don't run with debug turned on in production!
@@ -134,4 +136,4 @@ LOGOUT_REDIRECT_URL = 'notes:login'       # where to go after logout
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-GOOGLE_API_KEY = api
+GOOGLE_API_KEY = os.environ.get('OPENAI_API_KEY')
